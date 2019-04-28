8df1b4f2c49c4d4125c2c8eac8e513a04ef7973d
adehad <26027314+adehad@users.noreply.github.com>
2019-04-28 12:35:52 +0100

    submission folder created

--- BMI_MonkeyBusiness_Submission/MonkeyBusiness/positionEstimatorTraining.m
+++ BMI_MonkeyBusiness_Submission/MonkeyBusiness/positionEstimatorTraining.m
@@ -1,5 +1,5 @@
 %%% Team Members: Adel Haddad, Aishwarya Pattar, Alex Dack, Shafa Balaram
-%%% BMI Spring 2019 (Update 17th March 2015)
+%%% BMI Spring 2019 (Update 19th March 2019)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %         PLEASE READ BELOW            %
@@ -38,8 +38,6 @@
 % 
 % [x, y] = positionEstimator(trial(1,1), modelParameters);
 
-
-% model parameters: mean xTrain, weights and selected neurons, cluster centres
 function [modelParameters] = positionEstimatorTraining(training_data)
 % Arguments:
 
@@ -91,14 +89,14 @@ set(groot,  'DefaultLineLineWidth', 2.5, ...
             'DefaultAxesFontName', 'LaTeX', ...
             'DefaultLegendInterpreter', 'LaTeX', ...
             'DefaultAxesLineWidth', 1.5, ...
-            'DefaultAxesFontSize', 24, ...
+            'DefaultAxesFontSize', 18, ...
             'DefaultAxesFontName', 'Palatino');
 
 % region of interest
 tBeginTarget = 1; tEndTarget = 300;
 % nearest-centroid classification for angles
 [clusterCentre, meanXTrain] = trainAngleClassification(training_data, tBeginTarget, tEndTarget);
-[W, neuronSel, neuronPref] = popCodingTrain(training_data);
+[W, neuronSel, neuronPref, ref] = popCodingTrain(training_data);
 
 % output structure containing training parameters
 modelParameters = struct;
@@ -107,67 +105,12 @@ modelParameters.neuronSel = neuronSel;
 modelParameters.neuronPref = neuronPref;
 modelParameters.clusterCentre = clusterCentre;
 modelParameters.trainCentre = meanXTrain;
-end
-
-function [x, y] = positionEstimator(test_data, modelParameters)
-
-% **********************************************************
-%
-% You can also use the following function header to keep your state
-% from the last iteration
-%
-% function [x, y, newModelParameters] = positionEstimator(test_data, modelParameters)
-%                 ^^^^^^^^^^^^^^^^^^
-% Please note that this is optional. You can still use the old function
-% declaration without returning new model parameters.
-%
-% *********************************************************
-
-% - test_data:
-%     test_data(m).trialID
-%         unique trial ID
-%     test_data(m).startHandPos
-%         2x1 vector giving the [x y] position of the hand at the start
-%         of the trial
-%     test_data(m).decodedHandPos
-%         [2xN] vector giving the hand position estimated by your
-%         algorithm during the previous iterations. In this case, N is
-%         the number of times your function has been called previously on
-%         the same data sequence.
-%     test_data(m).spikes(i,t) (m = trial id, i = neuron id, t = time)
-%     in this case, t goes from 1 to the current time in steps of 20
-%     Example:
-%         Iteration 1 (t = 320):
-%             test_data.trialID = 1;
-%             test_data.startHandPos = [0; 0]
-%             test_data.decodedHandPos = []
-%             test_data.spikes = 98x320 matrix of spiking activity
-%         Iteration 2 (t = 340):
-%             test_data.trialID = 1;
-%             test_data.startHandPos = [0; 0]
-%             test_data.decodedHandPos = [2.3; 1.5]
-%             test_data.spikes = 98x340 matrix of spiking activity
-
-
-
-% ... compute position at the given timestep.
-
-% Return Value:
-
-% - [x, y]:
-%     current position of the hand
-
-tBeginTarget = 1; tEndTarget = 300;
-[predAngle] = testAngleClassification(test_data, tBeginTarget, tEndTarget, modelParameters.clusterCentres, modelParameters.trainCentre);
-modelParameters.predAngle = predAngle;
-
-[x,y] = popCoding_estimator(test_data, modelParameters);
-fprintf('\nPredicted X:%.4f \t Y:%.4f \n',x,y)
-
+modelParameters.ref = ref;
+modelParameters.predAngle = [];
 end
 
 %%% ---- Least Mean Square (LMS) Filter for Trajectory Prediction ---- %%%
-function [W, neuronSel, neuronPref] = popCodingTrain(trial)
+function [W, neuronSel, neuronPref, ref] = popCodingTrain(trial)
     %%  Initialise some variables
 
     % Reaching Angle set
@@ -235,10 +178,10 @@ function [W, neuronSel, neuronPref] = popCodingTrain(trial)
     end % end neuron
 
     % minT = shortest trial duration, 1=normal, 2=padded
-    minT(1) = min(lengthMatrix,[],'all');
+    minT(1) = min(min(lengthMatrix));
     minT(2) = activitySeg* ceil(minT(1)/activitySeg);
 
-    maxT(1) = max(lengthMatrix,[],'all');
+    maxT(1) = max(max(lengthMatrix));
     maxT(2) = activitySeg* ceil(maxT(1)/activitySeg);
 
     meanMatMean = zeros(numAngles, numNeurons);
@@ -388,17 +331,25 @@ function [W, neuronSel, neuronPref] = popCodingTrain(trial)
     end
 
     %% Population Coding
-    mu = 6;
+    % initial step-size
+    mu0 = 0.5;
+    epsilon = 0.02; % small regularisation term used to avoid division by zero 
 
     % reference signals
     ref.x = squeeze(Traj.mean(:,1,:));
     ref.y = squeeze(Traj.mean(:,2,:));
+    ref.xStd = squeeze(Traj.std(:,1,:));
+    ref.yStd = squeeze(Traj.std(:,2,:));
+    ref.xStep = max(abs(diff(ref.x,1,2)),[],2);
+    ref.yStep = max(abs(diff(ref.y,1,2)),[],2);
 
     clear pred err
 
     gdNeur = length(neuronSel); % number of good neurons
     dataLen = length(ref.x);
 
+    expFilterPrevWeight = 0.6;
+        
     % iterate over time
     for incAngle = 1:numAngles    
         % Each neuron has an associated weight
@@ -428,6 +379,11 @@ function [W, neuronSel, neuronPref] = popCodingTrain(trial)
             end
             predX(n) = predX(n)/length(neuronSel);
             predY(n) = predY(n)/length(neuronSel);
+            
+%             if n>1
+%                 predX(n) = expFilterPrevWeight*predX(n-1) + (1-expFilterPrevWeight)*predX(n);
+%                 predY(n) = expFilterPrevWeight*predY(n-1) + (1-expFilterPrevWeight)*predY(n);
+%             end
 
             % prediction error n, e(n)
             errX(n) = refX(n) - predX(n);
@@ -435,12 +391,15 @@ function [W, neuronSel, neuronPref] = popCodingTrain(trial)
 
             % weights update rule
             for jj=1:gdNeur % using selected neurons
-                W.x{incAngle}(jj,n+1) = W.x{incAngle}(jj,n) + mu * errX(n) * tuningCurve{neuronSel(jj)}.meanVecMean(incAngle,n) ...
-                    *cosd(neuronPref(jj))*(activitySeg) ...
-                    ;
-                W.y{incAngle}(jj,n+1) = W.y{incAngle}(jj,n) + mu * errY(n) * tuningCurve{neuronSel(jj)}.meanVecMean(incAngle,n) ...
-                    *sind(neuronPref(jj))*(activitySeg) ...
-                    ;
+                % update the step-size
+                muX =  mu0 / (norm(tuningCurve{neuronSel(jj)}.meanVecMean(incAngle,n)*cosd(neuronPref(jj))*(activitySeg))^2 + epsilon);
+                W.x{incAngle}(jj,n+1) = W.x{incAngle}(jj,n) + muX * errX(n) * tuningCurve{neuronSel(jj)}.meanVecMean(incAngle,n) ...
+                                                    *cosd(neuronPref(jj))*(activitySeg) ...
+                                                    ;
+                muY = mu0 / (norm(tuningCurve{neuronSel(jj)}.meanVecMean(incAngle,n)*sind(neuronPref(jj))*(activitySeg))^2 + epsilon);
+                W.y{incAngle}(jj,n+1) = W.y{incAngle}(jj,n) + muY * errY(n) * tuningCurve{neuronSel(jj)}.meanVecMean(incAngle,n) ...
+                                                    *sind(neuronPref(jj))*(activitySeg) ...
+                                                    ;
             end
 
         end
@@ -454,7 +413,8 @@ function [W, neuronSel, neuronPref] = popCodingTrain(trial)
 
     end
 
-    plotLMS = true;
+    maxLen = maxT(1);
+    plotLMS = false;
     whatAngle = 1;  % max value = numAngles
     whatNeuron = 1; % max value = length(neuronSel), can be an array, e.g. 1:5
 
@@ -483,199 +443,6 @@ function [W, neuronSel, neuronPref] = popCodingTrain(trial)
     end
 
 end
-
-%%% ---- Least Mean Square (LMS) Filter for Trajectory Prediction ---- %%%
-function [x,y] = popCoding_estimator(trial, modelParameters)
-    %%  Initialise some variables
-
-    % Reaching Angle set
-    angleInc = (40/180)*pi;
-    reachAngles = [(30/180)*pi:angleInc:(350/180)*pi];
-    reachAngles = reachAngles(reachAngles ~= (270/180)*pi );
-    regionOfInterest = [300, 100]; % (1): movement onset sample, (2) samples to subtract from end
-    numNeurons = size(trial(1,1).spikes,1);
-    numAngles = size(trial,2);
-    numTrials = size(trial,1);
-    activitySeg = 20; % we use to split into batches of 20 elements
-
-    % pre-allocate for speed
-    tuningCurve = cell(numNeurons,1); % an element for each neuron
-    lengthMatrix = zeros(size(trial)); % store the duration of everything
-
-    % Go through region of interest and store mean and variance per trial
-    for incNeuron=1:numNeurons        % for each neuron
-        for incAngle=1:numAngles      % for a given angle
-            for incTrial=1:numTrials  % across all trials
-
-                % Temporary Store spikes from region of interest
-                tempSpikes = trial(incTrial,incAngle).spikes(incNeuron,regionOfInterest(1):end-regionOfInterest(2));
-
-                % Store duration of region of interest
-                lengthMatrix(incTrial,incAngle) = length(tempSpikes);
-
-                % Store Mean (i.e. average neural activity)
-                tuningCurve{incNeuron}.meanMat(incTrial,incAngle) = ...
-                    mean( tempSpikes ,2);
-
-                % Store Standard Deviation (i.e. changes in activity)
-                tuningCurve{incNeuron}.stdMat(incTrial,incAngle) = ...
-                    std( tempSpikes, [] ,2);
-
-                % Temporary Store spikes from region of interest, add two extra
-                % chunks of activitySeg of just before the region of interest.
-                % We want the 'activity' from 20ms before the start, so we need
-                % an extra 20ms before that point (40ms) to calculate the 'activity'.
-                % 20ms = activitySeg
-                tempSpikes = trial(incTrial,incAngle).spikes(incNeuron,regionOfInterest(1)-2*activitySeg-1:end-regionOfInterest(2));
-
-                % nan-pad the tempSpikes variable (row vector)
-                tempSpikes = [ tempSpikes, ...
-                    nan(1, activitySeg*ceil(length(tempSpikes)/activitySeg)-length(tempSpikes) ) ];
-
-                % reshape the tempSpikes variable into batches of activitySeg
-                %             tempSpikes = reshape(tempSpikes, activitySeg, []); % OLD
-                % now we take a moving window of size activitySeg
-                tempSpikes2 = zeros(activitySeg, activitySeg*ceil(length(tempSpikes)/activitySeg));
-                for ii=1:lengthMatrix(incTrial,incAngle)+activitySeg
-                    tempSpikes2(:,ii) = tempSpikes(ii:ii+activitySeg-1)';
-                end
-
-                % Store Mean in 20ms segments (i.e. current neural activity)
-                tuningCurve{incNeuron}.meanVec{incTrial,incAngle} = ...
-                    mean( tempSpikes2, 'omitnan' );
-
-                % Store Standard Deviation in 20ms  (i.e. current variation in activity)
-                tuningCurve{incNeuron}.stdVec{incTrial,incAngle} = ...
-                    std( tempSpikes2, 'omitnan' );
-
-            end % end trial
-        end % end angle
-    end % end neuron
-
-    % minT = shortest trial duration, 1=normal, 2=padded
-    minT(1) = min(lengthMatrix,[],'all');
-    minT(2) = activitySeg* ceil(minT(1)/activitySeg);
-
-    maxT(1) = max(lengthMatrix,[],'all');
-    maxT(2) = activitySeg* ceil(maxT(1)/activitySeg);
-
-    meanMatMean = zeros(numAngles, numNeurons);
-    stdMatMean  = zeros(numAngles, numNeurons);
-
-    clear Traj
-    % average across trials
-    for incNeuron=1:numNeurons        % for each neuron
-        tuningCurve{incNeuron}.meanVecMean = zeros(numAngles, minT(1)+activitySeg);
-        tuningCurve{incNeuron}.stdVecMean  = zeros(numAngles, minT(1)+activitySeg);
-        for incAngle=1:numAngles      % for a given angle
-            for incTrial=1:numTrials  % across all trials
-
-                % Store Total Mean (i.e. average neural activity across trials)
-                meanMatMean(incAngle,incNeuron) = ...
-                    meanMatMean(incAngle,incNeuron) +  tuningCurve{incNeuron}.meanMat(incTrial,incAngle);
-
-                % Store Total Standard Deviation (i.e. average changes in activity across trials)
-                stdMatMean(incAngle,incNeuron) = ...
-                    stdMatMean(incAngle,incNeuron) +  tuningCurve{incNeuron}.stdMat(incTrial,incAngle);
-
-                % Store Total of the 20ms moving window (i.e. current neural activity)
-                % an extra +activitySeg is because we added the extra chunk earlier
-                % which we use later to predict the 300ms point
-                tuningCurve{incNeuron}.meanVecMean(incAngle,:) = ...
-                    tuningCurve{incNeuron}.meanVecMean(incAngle,:) +  tuningCurve{incNeuron}.meanVec{incTrial,incAngle}(1:minT(1)+activitySeg);
-
-                tuningCurve{incNeuron}.stdVecMean(incAngle,:) = ...
-                    tuningCurve{incNeuron}.stdVecMean(incAngle,:) +  tuningCurve{incNeuron}.stdVec{incTrial,incAngle}(1:minT(1)+activitySeg);
-
-                if incNeuron == 1 % only do this once
-                    % Store Total Tracjectory
-                    % we take 20ms - i.e. activitySeg before the start of the
-                    % interest area
-                    Traj.All(incTrial,incAngle,:,:) = trial(incTrial,incAngle).handPos(1:2,regionOfInterest(1)-activitySeg+1:regionOfInterest(1)+minT(1)); %1=x,2=y
-                    Traj.All_padded(incTrial,incAngle,:,:) = [ trial(incTrial,incAngle).handPos(1:2,regionOfInterest(1)-activitySeg+1:regionOfInterest(1)+lengthMatrix(incTrial,incAngle)), ...
-                        nan(2,maxT(1)-lengthMatrix(incTrial,incAngle)) ]; %1=x,2=y
-                elseif incNeuron == 2 % only do this once
-                    Traj.mean = squeeze(mean(Traj.All,1));
-                    Traj.std  = squeeze(std(Traj.All,[],1));
-                    Traj.mean_padd = squeeze(mean(Traj.All_padded,1, 'omitnan'));
-                    Traj.std_padd  = squeeze(std(Traj.All_padded,[],1, 'omitnan'));
-                end
-            end % end trial
-
-            % Convert Total to Averages
-            meanMatMean(incAngle,incNeuron) = meanMatMean(incAngle,incNeuron) / incTrial;
-            stdMatMean(incAngle,incNeuron) = stdMatMean(incAngle,incNeuron) / incTrial;
-            tuningCurve{incNeuron}.meanVecMean(incAngle,:) = tuningCurve{incNeuron}.meanVecMean(incAngle,:) / incTrial;
-            tuningCurve{incNeuron}.stdVecMean(incAngle,:) = tuningCurve{incNeuron}.stdVecMean(incAngle,:) / incTrial;
-
-        end % end angle
-    end % end neuron
-
-
-    %% Neuron Selection
-    neuronSel = modelParameters.neuronSel;
-    neuronPref = modelParameters.neuronPref;
-    %% Population Coding
-    mu = 6;
-    
-    clear pred
-
-    gdNeur = length(neuronSel); % number of good neurons
-    
-    % Each neuron has an associated weight
-    W = modelParameters.W;
-
-    
-    dataLen = minT(1);
-    
-    % iterate over time
-    for incAngle = modelParameters.predAngle    
-
-        % initialise some vars
-        predX = zeros(1,dataLen);
-        predY = predX;
-
-        for n=1:dataLen
-            
-            if n < size(W.x{incAngle},2)
-        
-
-            % initialise first prediction with bad weights
-            for jj=1:gdNeur % using selected neurons
-                predX(n) = predX(n) + W.x{incAngle}(jj,n)*tuningCurve{neuronSel(jj)}.meanVecMean(incAngle,n) ...
-                    *cosd(neuronPref(jj))*(activitySeg) ...
-                    ;
-                predY(n) = predY(n) + W.y{incAngle}(jj,n)*tuningCurve{neuronSel(jj)}.meanVecMean(incAngle,n) ...
-                    *sind(neuronPref(jj))*(activitySeg) ...
-                    ;
-            end
-            predX(n) = predX(n)/length(neuronSel);
-            predY(n) = predY(n)/length(neuronSel);
-
-            else
-                % Stops predicting once we run out of weights
-                
-            end
-        end
-
-        % Store weights for this trial
-        % cols = time. rows = angles.
-        pred.x(incAngle,:) = predX;
-        pred.y(incAngle,:) = predY;
-
-    end
-
-    x = pred.x(incAngle,:);
-    y = pred.y(incAngle,:);
-    
-    x = x(x ~=0); 
-    y = y(y ~=0);
-    
-    x = x(end);
-    y = y(end);
-
-end
-
 
 function [clusterCentre, meanXTrain] = trainAngleClassification(trials, tBeginTarget, tEndTarget)
     [metricPreMovement labels] = formatPreMovement(trials, tBeginTarget, tEndTarget);
@@ -724,43 +491,5 @@ function [clusterCentre, meanXTrain] = trainAngleClassification(trials, tBeginTa
             upperIndex = numTrials*angle;
             clusterCentre(angle,:) = mean(xTrainNorm(lowerIndex:upperIndex, :));
         end
-    end
-end
-function [predAngle] = testAngleClassification(trial, tBeginTarget, tEndTarget, clusterCentre, meanXTrain)
-    [metricPreMovementTest labels] = formatPreMovement(trial, tBeginTarget, tEndTarget);
-    preCrtTest = metricPreMovementTest - meanXTrain;
-    predAngle = nearestCentroid(clusterCentre, preCrtTest)';
-    function [outputTrainMetric, outputLabel] = formatPreMovement(trials, tLow, tHigh)
-        numTrials = size(trials,1);
-        numAngle = size(trials,2);
-        numNeuron = size(trials(1,1).spikes,1);
-        numMetrics = 2;
-        tDiff = tHigh - tLow; 
-        N = numTrials*numAngle;
-        D = tDiff*numNeuron;
-
-        outputTrainMetric = zeros(numTrials, numAngle, numMetrics*numNeuron);
-        outputLabel = zeros(numTrials, numAngle);
-
-        for trial = 1:numTrials
-            for angle = 1:numAngle
-                metricStore = zeros(numNeuron, numMetrics);
-                for neuron = 1:numNeuron
-                    lenSpike = length(trials(trial,angle).spikes(neuron,:));
-                    if lenSpike < tHigh-1
-                        metricStore(neuron,:) = [mean(trials(trial,angle).spikes(neuron , tLow:lenSpike)), std(trials(trial,angle).spikes(neuron , tLow:lenSpike))];
-                    else
-                        metricStore(neuron,:) = [mean(trials(trial,angle).spikes(neuron , tLow:tHigh-1)), std(trials(trial,angle).spikes(neuron , tLow:tHigh-1))];
-                    end                
-                end
-                outputTrainMetric(trial, angle, :) = metricStore(:);
-                outputLabel(trial, angle) = angle;
-            end
-        end
-        outputTrainMetric = reshape(outputTrainMetric, [N numMetrics*numNeuron]);
-        outputLabel = reshape(outputLabel, [N 1]);
-    end
-    function [iProj] = nearestCentroid(centroids, xTest)
-        [distProj iProj] = pdist2(centroids,xTest,'Euclidean' ,'Smallest', 1);
     end
 end
